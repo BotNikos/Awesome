eb75ccd2da5de4674202f0c88a143236a17f4abc
BotNikos <boss.nikitos1212@mail.ru>
2024-01-24 01:28:39 +0300

    Created colors file to access from anywhere

---
+++ colors.lua
@@ -0,0 +1,15 @@
+-- Временное решение
+-- Создавать отдельный файл для этого - кринж
+-- Надо вынести эти цвета в файл theme.lua, который и отвечает за это
+
+colors = {}
+
+colors.background = "#282A36"
+colors.background2 = "#383A59"
+colors.foreground = "#F4F4EF"
+colors.violet = "#BD93F9"
+colors.orange = "#FF9C32"
+colors.majenta = "#FF79C6"
+colors.blue = "#7CCCDF"
+
+return colors
